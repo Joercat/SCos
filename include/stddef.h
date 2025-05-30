8827e0bf683f629e22900856e92860368317e573
Joe <167563398+Joercat@users.noreply.github.com>
2025-05-30 19:42:47 -0400

    Add files via upload

---
+++ include/stddef.h
@@ -0,0 +1,32 @@
+#pragma once
+
+typedef unsigned int size_t;
+typedef int ptrdiff_t;
+typedef long intptr_t;
+typedef unsigned long uintptr_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef signed char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int64_t;
+
+#define NULL ((void*)0)
+#define nullptr NULL
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void* memset(void* ptr, int value, size_t size);
+void* memcpy(void* dest, const void* src, size_t size);
+int memcmp(const void* ptr1, const void* ptr2, size_t size);
+size_t strlen(const char* str);
+char* strcpy(char* dest, const char* src);
+int strcmp(const char* str1, const char* str2);
+
+#ifdef __cplusplus
+}
+#endif
