266112d9232c7418096e8a2cd52eb3d715030262
Joe <167563398+Joercat@users.noreply.github.com>
2025-08-09 23:01:51 -0400

    Update stdbool.h

--- SCos/include/stdbool.h
+++ SCos/include/stdbool.h
@@ -8,17 +8,17 @@
 #define true 1
 #define false 0
 
-#else /* __cplusplus */
+#else 
 
-/* Supporting <stdbool.h> in C++ is a GCC extension.  */
+
 #define _Bool bool
 #define bool bool
 #define false false
 #define true true
 
-#endif /* __cplusplus */
+#endif 
 
-/* Signal that all the definitions are present.  */
+
 #define __bool_true_false_are_defined 1
 
-#endif /* _STDBOOL_H */
+#endif l
