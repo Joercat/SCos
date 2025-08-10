7b4969af2529a8cffc180f059053d021e5f40f9a
Joe <167563398+Joercat@users.noreply.github.com>
2025-08-09 23:04:42 -0400

    Update ramfs.hpp

--- SCos/fs/ramfs.hpp
+++ SCos/fs/ramfs.hpp
@@ -4,7 +4,7 @@
 #ifndef RAMFS_HPP
 #define RAMFS_HPP
 
-// Function declarations for RAM filesystem
+
 bool initFS();
 const char* readFile(const char* path);
 bool writeFile(const char* path, const char* data);
