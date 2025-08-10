8df486e02c97db74e8aaacb5e719eacb029bf0f9
Joe <167563398+Joercat@users.noreply.github.com>
2025-08-09 23:06:27 -0400

    Update mouse.hpp

--- SCos/drivers/mouse.hpp
+++ SCos/drivers/mouse.hpp
@@ -3,25 +3,21 @@
 
 #include <stdint.h>
 
-// Mouse button states
-#define MOUSE_LEFT_BUTTON   0x01
-#define MOUSE_RIGHT_BUTTON  0x02
+#define MOUSE_LEFT_BUTTON 0x01
+#define MOUSE_RIGHT_BUTTON 0x02
 #define MOUSE_MIDDLE_BUTTON 0x04
 
-// Theme defines
 #define MATRIX_GREEN 1
 #define MATRIX_RED 2
 #define MATRIX_PURPLE 3
 
-// Default theme
 #ifndef THEME
 #define THEME MATRIX_GREEN
 #endif
 
-// New theme defines based on user request
-#define BACKGROUND_COLOR 0x000000 // Black
-#define TEXT_COLOR       0x39FF14 // Matrix Green
-#define SECONDARY_COLOR  0x39FF14 // Matrix Green
+#define BACKGROUND_COLOR 0x000000
+#define TEXT_COLOR 0x39FF14
+#define SECONDARY_COLOR 0x39FF14
 
 struct MouseState {
     int x, y;
@@ -37,10 +33,8 @@ private:
     static bool initialized;
 
 public:
-    // Initialization
     static bool init();
 
-    // State queries
     static MouseState getState();
     static int getX() { return current_state.x; }
     static int getY() { return current_state.y; }
@@ -48,33 +42,26 @@ public:
     static bool isRightButtonPressed() { return current_state.buttons & MOUSE_RIGHT_BUTTON; }
     static bool isMiddleButtonPressed() { return current_state.buttons & MOUSE_MIDDLE_BUTTON; }
 
-    // Event detection
     static bool wasLeftButtonClicked();
     static bool wasRightButtonClicked();
     static bool wasMiddleButtonClicked();
 
-    // Update functions
     static void update();
     static void handleMousePacket(uint8_t packet1, uint8_t packet2, uint8_t packet3);
 
-    // Mouse cursor drawing
     static void drawCursor();
     static void hideCursor();
 
-    // Bounds checking
     static void clampPosition();
 
-    // PS/2 mouse commands
     static void sendCommand(uint8_t command);
     static uint8_t readData();
     static void enableScrollWheel();
 
-    // Set mouse position
     static void setPosition(int x, int y);
 
 };
 
-// Mouse interrupt handler
 extern "C" void mouse_handler();
 
 #endif // MOUSE_HPP
