f5c7f2922ed63de1c4a5b983eb9b8550c4daec33
Joe <167563398+Joercat@users.noreply.github.com>
2025-08-09 22:39:48 -0400

    Update desktop.hpp

--- SCos/ui/desktop.hpp
+++ SCos/ui/desktop.hpp
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-// Forward declarations
 class ThemeManager;
 class WindowManager;
 class AppLauncher;
@@ -18,7 +17,6 @@ struct Window;
 #include "theme_manager.hpp"
 #include <stdint.h>
 
-// Forward declarations for application classes
 class Terminal;
 class Notepad;
 class FileManager;
@@ -34,6 +32,59 @@ enum AppType {
   APP_ABOUT,
   APP_FILE_MANAGER,
   APP_CALCULATOR,
+  APP_SECURITY,
+  APP_BROWSER,
+  APP_APP_STORE,
+  APP_UPDATES,
+  APP_NETWORK_SETTINGS
+};
+
+#define KEY_ALT 0x38
+#define KEY_TAB 0x0F
+#define KEY_CTRL 0x1D
+
+class Desktop {
+public:
+  static bool init();
+  static void handle_events();
+  static void update();
+  static void handleMouseInput();
+  static void handleTaskbarClick(int x);
+  static const char *readFile(const char *path);
+  static void launchApplication(AppType app);
+
+private:
+  static void drawDesktopBackground();
+  static void drawTaskbar();
+  static void drawActiveApps();
+  static void setupDefaultWindows();
+  static void run();
+  static void handleInput();
+  static void updateDesktop();
+  static void switchToNextWindow();
+  static void closeActiveWindow();
+  static void passInputToApplication(int window_id, uint8_t key);
+
+  static void openNotepad(const char *content);
+  static void runTerminal();
+  static void openFileManager();
+  static void openCalendar();
+  static void openSettings();
+  static void openAbout();
+  static void launchCalculator();
+  static void openSecurityCenter();
+  static void openBrowser();
+  static void openAppStore();
+
+  static void drawOpenAppIcons(int start_x, int y);
+  static char getAppIcon(const char* title);
+
+  static void performSearch(const char* query);
+  static char getCharFromScancode(uint8_t scancode);
+
+};
+
+#endif
   APP_SECURITY,
   APP_BROWSER,
   APP_APP_STORE,
