a2238b19afae58a8b626277912264a77c91fc095
Joe <167563398+Joercat@users.noreply.github.com>
2025-08-09 22:33:51 -0400

    Update window_manager.hpp

--- SCos/ui/window_manager.hpp
+++ SCos/ui/window_manager.hpp
@@ -34,7 +34,7 @@ private:
     static void clearWindowArea(int x, int y, int width, int height);
 };
 
-// VGA text mode functions
+
 void vga_clear_screen(uint8_t color);
 void vga_clear_line(int y, uint8_t color);
 void vga_put_char(int x, int y, char c, uint8_t color);
@@ -43,7 +43,7 @@ void vga_draw_box(int x, int y, int width, int height, uint8_t color);
 void center_text(int y, const char* text, uint8_t color);
 void draw_horizontal_line(int y, int x1, int x2, char c, uint8_t color);
 
-// Color constants
+
 #define COLOR_BLACK         0
 #define COLOR_BLUE          1
 #define COLOR_GREEN         2
@@ -63,7 +63,7 @@ void draw_horizontal_line(int y, int x1, int x2, char c, uint8_t color);
 
 #define MAKE_COLOR(fg, bg) ((bg << 4) | fg)
 
-// Screen dimensions (defined as constants in cpp file)
+
 extern const int VGA_WIDTH;
 extern const int VGA_HEIGHT;
 
