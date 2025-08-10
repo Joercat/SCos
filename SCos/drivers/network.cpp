dec6afb7c50ad50bb610e9cd93e57e6f270fdaa9
Joe <167563398+Joercat@users.noreply.github.com>
2025-08-09 23:05:38 -0400

    Update network.cpp

--- SCos/drivers/network.cpp
+++ SCos/drivers/network.cpp
@@ -1,13 +1,10 @@
-
 #include "network.hpp"
 #include "../debug/serial.hpp"
 
-// Static member definitions
 bool NetworkDriver::wifi_connected = false;
 char NetworkDriver::ip_address[16] = "192.168.1.100";
 char NetworkDriver::ssid[32] = "";
 
-// Simple string copy function
 static void safe_strcpy(char* dest, const char* src, int max_len) {
     int i = 0;
     while (src[i] && i < max_len - 1) {
@@ -32,10 +29,8 @@ bool NetworkDriver::isConnected() {
 bool NetworkDriver::connectToWifi(const char* network_ssid, const char* password) {
     serial_printf("Connecting to WiFi: %s\n", network_ssid);
     
-    // Simulate connection process
     safe_strcpy(ssid, network_ssid, 32);
     
-    // Mock successful connection
     wifi_connected = true;
     safe_strcpy(ip_address, "192.168.1.100", 16);
     
@@ -56,14 +51,12 @@ int NetworkDriver::sendData(const uint8_t* data, int length) {
     if (!wifi_connected) return -1;
     
     serial_printf("Sending %d bytes\n", length);
-    // Mock successful send
     return length;
 }
 
 int NetworkDriver::receiveData(uint8_t* buffer, int max_length) {
     if (!wifi_connected) return -1;
     
-    // Mock receiving some data
     const char* mock_data = "HTTP/1.1 200 OK\r\n\r\nHello World";
     int data_len = 0;
     while (mock_data[data_len] && data_len < max_length - 1) {
